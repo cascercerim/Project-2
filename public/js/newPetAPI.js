fd59bb9e5367ce8c7f2ac485d6131135d91a428d
Maria <cascercerim@gmail.com>
2019-12-04 18:09:16 -0500

    changing keys for api

--- public/js/newPetAPI.js
+++ public/js/newPetAPI.js
@@ -4,19 +4,12 @@ require("dotenv").config();
 // console.log(process.env);
 
 // dependencies
-let petfinder = require("@petfinder/petfinder-js");
-
-// Add the code required to import the `keys.js` file and store it in a variable
-const keys = require("./keys.js");
-let client = new petfinder.Client(keys.petfinder);
-
-// @petfinder API Call
-client.animal.search()
+var pf = new petfinder.Client({apiKey: "EGc3gxBpk6teIzgELn3yobiuQzodUCyieJnvK8zEj633m76rEz", secret: "TJgV9Um001Akix3x70lcFD1eO3gIQX7aAIBlIMUB"});
+ 
+pf.animal.search()
     .then(function (response) {
         // Do something with `response.data.animals`
-        console.log(response.data)
     })
     .catch(function (error) {
         // Handle the error
-        console.log(error);
     });
